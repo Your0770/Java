c4717cce63c4469bdbcf25a86a31762ca95e5a24
admin <admin@DESKTOP-1QPVEE5>
2024-10-01 12:31:42 +0900

    1001-4

--- hongongjava/src/chap07/FileDownloadServlet.java
+++ hongongjava/src/chap07/FileDownloadServlet.java
@@ -2,7 +2,7 @@ package chap07;
 
 public class FileDownloadServlet extends HttpServlet {
 	//field
-		 
+		
 	//constructor
 			
 	//method
--- hongongjava/src/chap07/HttpServlet.java
+++ hongongjava/src/chap07/HttpServlet.java
@@ -2,7 +2,7 @@ package chap07;
 
 public abstract class HttpServlet {
 	//field
-		 
+		
 	//constructor
 	
 	//method
