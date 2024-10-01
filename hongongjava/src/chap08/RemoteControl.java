1f42769354f4f527717b32fbeac9cb75e20d3e8a
admin <admin@DESKTOP-1QPVEE5>
2024-10-01 13:11:08 +0900

    1001-9

--- hongongjava/src/chap08/RemoteControl.java
+++ hongongjava/src/chap08/RemoteControl.java
@@ -2,9 +2,9 @@ package chap08;
 
 public interface RemoteControl {
 	//상수
-	public int MAX_VOLUEME = 10;
-	public int MIN_VOLUEME = 0;
-	
+	public int MAX_VOLUME = 10;
+	public int MIN_VOLUME = 0;
+	 
 	//추상메소드
 	public void turnOn();
 	public void turnOff();
